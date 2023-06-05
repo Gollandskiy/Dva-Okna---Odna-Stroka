6d59334a120850def9bb09366a289affc182fa15
nikita.prigolovkin <vitalii.prigolovkin@gmail.com>
2023-06-05 18:07:02 +0300

    Добавьте файлы проекта.

---
+++ Form1.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Dva_Okna___Odna_Stroka
+{
+    public partial class Form1 : Form
+    {
+        private Form2 form2;
+        public Form1()
+        {
+            InitializeComponent();
+            StartPosition = FormStartPosition.CenterScreen;
+            form2 = new Form2();
+            form2.Show();
+        }
+        public void ChangeText(string str)
+        {
+            textBox1.Text = str;
+        }
+
+        private void textBox1_KeyUp(object sender, KeyEventArgs e)
+        {
+            form2.ChangeText(textBox1.Text);
+        }
+    }
+}
---
+++ Form2.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement.TextBox;
+
+namespace Dva_Okna___Odna_Stroka
+{
+    public partial class Form2 : Form
+    {
+        private Form1 form1;
+        public Form2()
+        {
+            InitializeComponent();
+        }
+        public void ChangeText(string text)
+        {
+            textBox2.Text = text;
+        }
+
+        private void textBox2_KeyUp(object sender, KeyEventArgs e)
+        {
+            form1.ChangeText(textBox2.Text);
+        }
+    }
+}
